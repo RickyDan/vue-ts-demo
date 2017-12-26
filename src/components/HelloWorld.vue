47dee4d8ab8d40a0fcb4516e13d42293a9ff2e9c
RickyDan <tangzhiming90@gmail.com>
2017-12-26 19:57:00 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <Item name="typescript + vue"></Item>
+    <el-button type="primary" @click="add(3, 4)">点击</el-button>
   </div>
 </template>
 <script lang="ts">
@@ -15,5 +16,15 @@ import Item from './Item.vue'
 })
 export default class HelloWorld extends Vue {
   msg: string = 'WelCome to Your Vue-Typescript App'
+
+  created () {
+    console.log(this.msg)
+    this.add(1, 2)
+  }
+
+  add (a: number, b: number): number {
+    console.log(a + b)
+    return a + b
+  }
 }
 </script>
